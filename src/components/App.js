fb749835d3e91792b276defb955cc7010ebfa9e2
mazdak <nazemi.works@gmail.com>
2019-05-19 05:04:40 -0700

    starter with simple implementing

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  state = { language : 'english'}
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  }
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          select a language : 
+          <i className="flag us" onClick={()=>this.onLanguageChange('english')}/>
+          <i className="flag nl" onClick={()=>this.onLanguageChange('dutch')}/>
+        </div>
+        {this.state.language}
+      </div>
+    );
+  }
+}
+
+export default App;
