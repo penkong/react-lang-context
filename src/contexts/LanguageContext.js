1d8d289364cd6e5a24d0c64df16dc19623ea6ff0
mazdak <nazemi.works@gmail.com>
2019-05-19 09:16:16 -0700

    replacing redux with context make hoc with context and use as wrapper where we want

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,4 +1,22 @@
 import React from 'react';
 
+const Context = React.createContext('english');
+export class LanguageStore extends React.Component {
 
-export default React.createContext('english');
+  state = { language : 'english'};
+
+  onLanguageChange = (language) => {
+    this.setState({ language });
+  }
+
+  render(){
+    return (
+      <Context.Provider 
+      value={{...this.state , onLanguageChange : this.onLanguageChange}}>
+        {this.props.children}        
+      </Context.Provider>
+    );
+  }
+}
+
+export default Context;
