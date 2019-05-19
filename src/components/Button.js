697112165f57b323badcd2f6565dfbaadf27870b
mazdak <nazemi.works@gmail.com>
2019-05-19 06:15:49 -0700

    use multi context also app done

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class Button extends Component {
   //context Type connect comp to context obj
   //static means it is for comp itself
-  static contextType = LanguageContext;
+  // static contextType = LanguageContext;
   //now we have this.context here
-  render() {
-    const text = this.context === 'english' ? 'submit' : 'voorleggen';
+
+  renderButton(color){
     return (
-      <button className="ui primary button">{text}</button>
+      <button className={`ui ${color} button`}>
+        <LanguageContext.Consumer>
+          {(value)=>value === 'english' ? 'submit' : 'voorleggen'}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  }
+
+  render() {
+    // const text = this.context === 'english' ? 'submit' : 'voorleggen';
+    // child of consumer is always a func
+    return (
+        <ColorContext.Consumer>
+          {(color)=>this.renderButton(color)}
+        </ColorContext.Consumer>
+        
     );
   }
 }
