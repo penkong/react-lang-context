84173d79bca812cf31e80ce3beb6bf84da98c96a
mazdak <nazemi.works@gmail.com>
2019-05-19 05:58:58 -0700

    use context way 1 this.context

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends Component {
+  //context Type connect comp to context obj
+  //static means it is for comp itself
+  static contextType = LanguageContext;
+  //now we have this.context here
   render() {
+    const text = this.context === 'english' ? 'submit' : 'voorleggen';
     return (
-      <button className="ui primary button">Submit</button>
+      <button className="ui primary button">{text}</button>
     );
   }
 }
